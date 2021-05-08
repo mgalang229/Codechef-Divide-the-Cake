fab8c93ee947c9398e46556a9b47a06f6972e9f2
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-05-08 11:16:01 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		// check if 360 is divisble by 'n' to see if it's possible to make equal parts
+		cout << (360 % n == 0 ? 'y' : 'n') << " ";
+		// check if 'n' is less than or equal to 360 to see if it's possible to make 'n' parts
+		cout << (n <= 360 ? 'y' : 'n') << " ";
+		// check if the sum of all the angles is less than or equal to 360
+		cout << ((n * (n + 1)) / 2 <= 360 ? 'y' : 'n') << '\n';
+	}
+	return 0;
+}
